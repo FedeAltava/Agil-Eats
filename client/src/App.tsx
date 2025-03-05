910666315b032c7d3a1a37b89dfab61a5f3b9b13
Federico <fedealtava1992@gmail.com>
2025-03-05 18:29:57 +0100

    routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route } from "react-router-dom"
 import { Header } from "./components/Header"
 
 
@@ -9,6 +9,11 @@ function App() {
     <BrowserRouter>
       <h1>Hello World!</h1>
       <Header />
+      <Route path="/" element={<Home />}/>
+      <Route path="/login" element={<Home />}/>
+      <Route path="/register" element={<Home />}/>
+      <Route path="/restaurant/:id" element={<Home />}/>
+      <Route path="/cart" element={<Home />}/>
     </BrowserRouter>
   )
 }
