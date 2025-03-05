92ece8fa36a741d465d6ec0536f48f1b072d1c8b
Federico <fedealtava1992@gmail.com>
2025-03-05 20:13:17 +0100

    retaurant detaiils add+route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Navbar } from "./components/Header/Navbar.tsx";
+import { Navbar } from "./components/NavBar/Navbar.tsx";
 import { Home } from "./pages/Home/Home";
 import { Login } from "./pages/Login/Login";
 import { Register } from "./pages/Register/Register";
 import { Cart } from "./pages/Cart/Cart.tsx";
+import { RestaurantDetails } from "./components/RestauranDetails/RestaurantDetails.tsx";
 
 const App: React.FC = () => {
   return (
@@ -14,7 +15,7 @@ const App: React.FC = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/restaurant/:id" element={<Home />} />
+          <Route path="/restaurant/:id" element={<RestaurantDetails />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </main>
---
+++ client/src/components/RestauranDetails/RestaurantDetails.tsx
@@ -0,0 +1,7 @@
+export const RestaurantDetails = () => {
+  return (
+    <>
+      <h1>Restuarant Details</h1>
+    </>
+  );
+};
