bb16a01a9ab0a506af8dbb2ab1298a5a30f64f7d
Federico <fedealtava1992@gmail.com>
2025-03-06 20:09:21 +0100

    d

--- server/src/db.ts
+++ server/src/db.ts
@@ -9,7 +9,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI || "");
     console.log(`MongoDB conectado: ${conn.connection.host}`);
   } catch (error) {
-    console.error("Error al conectar MongoDB:", error);
+    console.error("Error al Conectar MongoDB:", error);
     process.exit(1);
   }
 };
