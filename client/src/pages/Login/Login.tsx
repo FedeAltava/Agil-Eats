ad5b6c491a55234f693bbf83836a7560624a73e4
Federico <fedealtava1992@gmail.com>
2025-03-11 16:03:27 +0100

    Login client done ?

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,15 +1,40 @@
 import { useState } from "react";
 import "./Login.css";
 export const Login = ()=>{
-    const [ email, setEmail ] = useState("")
-
+    const [ email, setEmail ] = useState<string>("")
+    const [passWord, setPassWord] = useState<string>("");
+    const [ error, setError] = useState<string | null>(null);
+    
+    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        if(!email || !passWord){
+            setError("All areas are mandatories");
+            return;
+        }
+        if (!/\S+@\S+\.\S+/.test(email)) {
+            setError("Email is not valid");
+            return;
+          }
+        try{
+            console.log(email,passWord)
+            setError(null)
+        }catch(error){
+            setError("login Error")
+        }
+    }
     return(
         <section>
-            <form>
-                <label htmlFor="">Email</label>
-                <input type="text" value={email} onChange={(event) =>setEmail(event.target.value)}/>
-                <label htmlFor=""> Password</label>
-                <input type="text" />
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="email">
+                    <label htmlFor="" className="">Email</label>
+                    <input type="text" value={email} onChange={(event) =>setEmail(event.target.value)}/>
+                </div>
+                <div className="passWord">
+                    <label htmlFor=""> Password</label>
+                    <input type="text" value={passWord} onChange={(event)=>setPassWord(event.target.value)}/>
+                </div>
+                {error && <p className="error">{error}</p>}
+                <button type="submit">Log in</button>
             </form>
         </section>
     )
--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,8 +1,31 @@
 import "./Register.css"
 export const Register =()=>{
     return(
-        <>
-            <h1>Register</h1>
-        </>
+        <section>
+            <div className="Name">
+                <label htmlFor="">Name</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label htmlFor="" className="Email">Email</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label htmlFor="" className="Password">Password</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label htmlFor="" className="Role">Role</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label htmlFor="" className="Address">Address</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label htmlFor="" className="Phone">Phone</label>
+                <input type="text" />
+            </div>
+        </section>
     )
 }
