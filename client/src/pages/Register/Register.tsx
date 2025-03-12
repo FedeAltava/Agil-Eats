8c97b95349488bebd0b4b85236b6c4cd5ae043ed
Federico <fedealtava1992@gmail.com>
2025-03-12 18:35:05 +0100

    register css

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,31 +1,48 @@
-import "./Register.css"
-export const Register =()=>{
-    return(
-        <section>
-            <div className="Name">
-                <label htmlFor="">Name</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label htmlFor="" className="Email">Email</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label htmlFor="" className="Password">Password</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label htmlFor="" className="Role">Role</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label htmlFor="" className="Address">Address</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label htmlFor="" className="Phone">Phone</label>
-                <input type="text" />
-            </div>
-        </section>
-    )
-}
+import "./Register.css";
+export const Register = () => {
+  return (
+    <section>
+      <div className="container">
+        
+        <form action="">
+        <h1>Register</h1>
+        <div className="Name column">
+          <label htmlFor="">Name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="" className="Email column">
+            Email
+          </label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="" className="Password column">
+            Password
+          </label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="" className="Role column">
+            Role
+          </label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="" className="Address column">
+            Address
+          </label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="" className="Phone column">
+            Phone
+          </label>
+          <input type="text" />
+        </div>
+        <button type="submit" className="submit">Register</button>
+        </form>
+      </div>
+    </section>
+  );
+};
